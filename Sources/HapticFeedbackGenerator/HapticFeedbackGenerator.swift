6832f47e5df5bb9253122c21cfc2e4a723016ab5
Tadeusz Raszka <tadeusz.raszka@idevband.com>
2022-08-24 14:03:07 +0200

    Make all methods public

--- Sources/HapticFeedbackGenerator/HapticFeedbackGenerator.swift
+++ Sources/HapticFeedbackGenerator/HapticFeedbackGenerator.swift
@@ -3,17 +3,17 @@ import UIKit
 
 public struct HapticFeedbackGenerator {
 
-    public init() { }
+//    public init() { }
 
     /// Generate impact feedback with selected style
     /// - Parameter style: UIImpactFeedbackGenerator.FeedbackStyle
-    func generateImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    public func generateImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
         UIImpactFeedbackGenerator(style: style).impactOccurred()
     }
 
     /// Generate notification feedback with selected style
     /// - Parameter style: UINotificationFeedbackGenerator.FeedbackType
-    func generateNotificationFeedback(style: UINotificationFeedbackGenerator.FeedbackType) {
+    public func generateNotificationFeedback(style: UINotificationFeedbackGenerator.FeedbackType) {
         UINotificationFeedbackGenerator().notificationOccurred(.error)
     }
 }
